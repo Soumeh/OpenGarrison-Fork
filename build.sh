be3d238b3b9dbe158cd7999b2606806e1f3f4970
Soumeh <soumeh@proton.me>
2026-03-17 23:38:27 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o ./dist/linux-x64
+#!/bin/bash
+set -e
+
+RID="${1:-linux-x64}"
+OUT="./dist/$RID"
+FLAGS="-c Release -r $RID --self-contained false -o $OUT"
+
+PROJECTS=(
+  "OpenGarrison.Client/OpenGarrison.Client.csproj"
+  "OpenGarrison.Server/OpenGarrison.Server.csproj"
+  "OpenGarrison.ServerLauncher/OpenGarrison.ServerLauncher.csproj"
+)
+
+for proj in "${PROJECTS[@]}"; do
+  dotnet publish "$proj" $FLAGS
+done
